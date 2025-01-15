a14f55691e19c74af970a80f62fe9fefa4d0b63a
viniciusmaldonado <viniciusmaldonado@gmail.com>
2025-01-15 20:02:49 -0300

    Added variable and provisioners to Terraform module aws_humangov_infrastructure/main.tf

--- terraform/modules/aws_humangov_infrastructure/main.tf
+++ terraform/modules/aws_humangov_infrastructure/main.tf
@@ -1,81 +1,92 @@
 resource "aws_security_group" "state_ec2_sg" {
-  name        = "humangov-${var.state_name}-ec2-sg"
-  description = "Allow traffic on ports 22 and 80"
-
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    security_groups = ["sg-08e1b1bc993622070"]
-  }
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "humangov-${var.state_name}"
-  }
+    name = "humangov-${var.state_name}-ec2-sg"
+    description = "Allow traffic on ports 22 and 80"
+    
+      ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+      
+      ingress {
+        from_port   = 5000
+        to_port     = 5000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    
+      ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        security_groups = ["sg-027f57abd3fefda49"]
+      }
+      
+      egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    
+    tags = {
+        Name = "humangov-${var.state_name}"
+    }
 }
 
 resource "aws_instance" "state_ec2" {
-  ami           = "ami-007855ac798b5175e"
-  instance_type = "t2.micro"
-  key_name = "humangov-ec2-key"
-  vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.s3_dynamodb_full_access_instance_profile.name
-
-  tags = {
-    Name = "humangov-${var.state_name}"
-  }
+    ami = "ami-007855ac798b5175e"
+    instance_type = "t2.micro"
+    key_name = "humangov-ec2-key"
+    vpc_security_group_ids = [aws_security_group.state_ec2_sg.id]
+    iam_instance_profile = aws_iam_instance_profile.s3_dynamodb_full_access_instance_profile.name
+    
+  provisioner "local-exec" {
+	  command = "sleep 30; ssh-keyscan ${self.private_ip} >> ~/.ssh/known_hosts"
+	}
+	
+	provisioner "local-exec" {
+	  command = "echo ${var.state_name} id=${self.id} ansible_host=${self.private_ip} ansible_user=ubuntu us_state=${var.state_name} aws_region=${var.region} aws_s3_bucket=${aws_s3_bucket.state_s3.bucket} aws_dynamodb_table=${aws_dynamodb_table.state_dynamodb.name} >> /etc/ansible/hosts"
+	}
+	
+	provisioner "local-exec" {
+	  command = "sed -i '/${self.id}/d' /etc/ansible/hosts"
+	  when = destroy
+	}
+    
+    tags = {
+        Name = "humangov-${var.state_name}"
+    }
 }
 
 resource "aws_dynamodb_table" "state_dynamodb" {
-  name           = "humangov-${var.state_name}-dynamodb"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-  tags = {
-    Name = "humangov-${var.state_name}"
-  }
+    name = "humangov-${var.state_name}-dynamodb"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "id"
+    
+    attribute {
+        name = "id"
+        type = "S"
+    }
+    
+    tags = {
+        Name = "humangov-${var.state_name}"
+    }    
 }
 
 resource "random_string" "bucket_suffix" {
-  length  = 4
-  special = false
-  upper = false
+    length = 4
+    special = false
+    upper = false
 }
 
 resource "aws_s3_bucket" "state_s3" {
-  bucket = "humangov-${var.state_name}-s3-${random_string.bucket_suffix.result}"
-  
-
-  tags = {
-    Name = "humangov-${var.state_name}"
-  }
+    bucket = "humangov-${var.state_name}-s3-${random_string.bucket_suffix.result}"
+    
+    tags = {
+        Name = "humangov-${var.state_name}"
+    }       
 }
 
 resource "aws_iam_role" "s3_dynamodb_full_access_role" {
--- terraform/modules/aws_humangov_infrastructure/variables.tf
+++ terraform/modules/aws_humangov_infrastructure/variables.tf
@@ -1,3 +1,10 @@
 variable "state_name"{
     description = "The name of the US State"
 }
+variable "state_name" {
+    description = "The name of the US State"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
